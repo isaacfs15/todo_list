a7bce5d16c6ba682b98003bc14845953c2e71a5c
Deivison Santana Calazans <deivisonsantana189@gmail.com>
2026-04-08 09:44:18 -0300

    Limpando todos os arquivos da branch deivison

--- task_service/src/routes/usuarioRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require ('express');;
-const router = express.Router();
-const UsuarioController = require('../controller/UsuarioController');
-
-router.post('/cadastro', UsuarioController.cadastrar);
-router.post('/login', UsuarioController.login);
-
-module.exports = router;
