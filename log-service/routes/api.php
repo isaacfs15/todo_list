87b007c9a399e11163abdfe17f07a46ee3bcbf33
isaacfs15 <isaacfs370@gmail.com>
2026-04-07 22:15:58 -0300

    Limpando todos os arquivos da branch isaac

--- log-service/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\LogController;
-use Illuminate\Support\Facades\Route;
-
-Route::post('/logs',             [LogController::class, 'store']);
-Route::get('/logs',              [LogController::class, 'index']);
-Route::get('/logs/{usuarioId}',  [LogController::class, 'porUsuario']);
